81f9f35b684762cd0cad65497c7efd87cc54ffe0
huhaha <123456785@qq.com>
2022-04-09 00:35:59 +0800

    master1

--- src/main/java/git/gitTest.java
+++ src/main/java/git/gitTest.java
@@ -6,5 +6,6 @@ public class gitTest {
         System.out.println("hello,git 1");
         System.out.println("hello,git 2");
         System.out.println("hot-fix 1");
+        System.out.println("hot-fix 2");
     }
 }
